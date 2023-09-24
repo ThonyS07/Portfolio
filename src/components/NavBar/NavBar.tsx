8e080c5c619500fff5a3bec4d969fe1462fcd128
ThonyS07 <og@MacBook-Pro-de-og.local>
2023-09-24 13:49:16 -0500

    adiciones al navber

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,19 @@
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
-import Link from "next/link"
-import styles from "./NavBar.module.css"
-
+import Link from "next/link";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
 	return (
 		<div className={styles.nav}>
-			
-			<Link href='/about'>About</Link>
-			<Link href='/projects'>Projects</Link>
-			<Link href='/contact'>Contact</Link>
+			<div className={styles.logo}>
+				<Link href='/'>logo</Link>
+			</div>
+			<div className={styles.routes}>
+				<Link href='/about'>About</Link>
+				<Link href='/projects'>Projects</Link>
+				<Link href='/contact'>Contact</Link>
+			</div>
+
 			<ThemeSwitcher />
 		</div>
 	);
