52b2bfd608619b079253fa7109d098e1198a6553
Andrea <andrea@Andreas-MacBook-Pro.local>
2024-04-20 21:58:02 -0500

    margin homecard

--- src/app/components/Cards/HomeCards/HomeCard.tsx
+++ src/app/components/Cards/HomeCards/HomeCard.tsx
@@ -5,16 +5,16 @@ import PRUEBAicon from "./Icons/PRUEBA";
 
 const HomeCard = ({ children,text, titulo }: { children: React.ReactNode, text: string, titulo: string }) => {
 	return (
-		<div className='w-[312px] h-[202px] lg:w-[212px] lg:h-[220px] min-[1360px]:w-[368px] min-[1360px]:h-[182px] p-[27px] rounded-[16px] dark:shadow-[4px_4px_20px_0_#b997f9] dark:bg-[#1a1d2c] shadow-[4px_4px_20px_0_#4b506b] '>
+		<div className='w-[312px] h-[202px]  lg:w-[212px] lg:h-[220px] min-[1360px]:w-[368px] min-[1360px]:h-[182px] p-[27px] rounded-[16px] dark:shadow-[4px_4px_20px_0_#b997f9] dark:bg-[#1a1d2c] shadow-[4px_4px_20px_0_#4b506b] '>
 			<div className=''>
 				<HomeIcon>{children}</HomeIcon>
 			</div>
-			<div className=''>
-				{" "}
+			<div className='my-[16px]'>
+				
 				<TextB1>{titulo}</TextB1>{" "}
 			</div>
 			<div className=''>
-				{" "}
+				
 				<TextB3>{text}</TextB3>{" "}
 			</div>
 		</div>
