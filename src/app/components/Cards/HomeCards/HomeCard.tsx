6c7086624e2fb003816b7ab44d0c0ef2f5db67fa
Anthony Silva <114630040+ThonyS07@users.noreply.github.com>
2024-04-20 21:25:22 -0500

    Update HomeCard.tsx

--- src/app/components/Cards/HomeCards/HomeCard.tsx
+++ src/app/components/Cards/HomeCards/HomeCard.tsx
@@ -5,7 +5,7 @@ import PRUEBAicon from "./Icons/PRUEBA";
 
 const HomeCard = ({ children,text, titulo }: { children: React.ReactNode, text: string, titulo: string }) => {
 	return (
-		<div className='w-[312px] h-[202px] lg:w-[212px] lg:h-[220px] min-[1360px]:w-[368px] min-[1360px]:h-[182px] p-[27px] rounded-[16px] dark:shadow-[4px_4px_20px_0_#b997f9] dark:bg-[#1a1d2c] shadow-[4px_4px_20px_0_#4b506b66] '>
+		<div className='w-[312px] h-[202px] lg:w-[212px] lg:h-[220px] min-[1360px]:w-[368px] min-[1360px]:h-[182px] p-[27px] rounded-[16px] dark:shadow-[4px_4px_20px_0_#b997f9] dark:bg-[#1a1d2c] shadow-[4px_4px_20px_0_#4b506b] '>
 			<div className=''>
 				<HomeIcon>{children}</HomeIcon>
 			</div>
