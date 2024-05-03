9388dbffd3bbf0dcbe91c287c56997bf7f3fed04
Thony Silva <thonysilva1993@gmail.com>
2024-05-02 22:40:32 -0500

    eliminando problemas de BD

--- src/app/interfaces/Project.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from "@prisma/client";
-
-export type CreateProject = Omit<Project, "id" | "createdAt" | "updatedAt">;
-
-export type UpdateProject = Partial<CreateProject>;
-
-export interface Params {
-	params: { id: string };
-}
