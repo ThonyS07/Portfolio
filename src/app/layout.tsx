b0f036ff5b281a52ee091e58a087beb8cf421dea
Thony Silva <thonysilva1993@gmail.com>
2024-10-03 16:09:04 -0500

    fixing bug in page Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,13 +32,13 @@ export default function RootLayout({
 					rel='icon'
 					type='image/png'
 					sizes='32x32'
-					href='/favicon-32x32.png'
+					href='/favicon 32.ico'
 				/>
 				<link
 					rel='icon'
 					type='image/png'
 					sizes='16x16'
-					href='/favicon-16x16.png'
+					href='/favicon (16).ico'
 				/>
 				<link rel='manifest' href='/site.webmanifest' />
 			</head>
